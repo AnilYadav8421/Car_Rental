2e69e569b62e63662924641b70ecdb491761753e
Anil Yadav <anilyadav8149838058@gmail.com>
2025-08-11 21:41:01 +0530

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,14 +10,29 @@ const Hero = () => {
             <form className='flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]'>
 
                 <div className='flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-8'>
-                    <div>
+                    <div className='flex flex-col items-start gap-2'>
                         <select required value={pickupLocation} onChange={(e) => setPickUpLocation(e.target.value)}>
                             <option>Pickup Location</option>
                             {cityList.map((city) => <option key={city} value={city}>{city}</option>)}
                         </select>
                         <p className='px-1 text-sm text-gray-500'>{pickupLocation ? pickupLocation : 'Please select Location'}</p>
                     </div>
+
+                    <div className='flex flex-col items-start gap-2'>
+                        <label htmlFor="pickup-date">Pick-up Date</label>
+                        <input type="date" id="pickup-date" min={new Date().toISOString().split('T')[0]} className='text-sm text-gray-500' required />
+                    </div>
+
+                    <div className='flex flex-col items-start gap-2'>
+                        <label htmlFor="return-date">Return Date</label>
+                        <input type="date" id="return-date" className='text-sm text-gray-500' required />
+                    </div>
+
                 </div>
+                <button className='flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary hover:bg-primary-dull text-white rounded-full cursor-pointer' >
+                    Search
+                    <img src={assets.search_icon} alt="search" className='brightness-300' />
+                </button>
             </form>
 
             <img src={assets.main_car} alt="main car" className='max-h-74' />

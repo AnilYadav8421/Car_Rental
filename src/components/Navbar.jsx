1599cf35f42c816c1a3d686e973035ee7ca362ec
Anil Yadav <anilyadav8149838058@gmail.com>
2025-08-11 16:35:05 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import { assets, menuLinks } from '../assets/assets'
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
     const location = useLocation();
     const [isOpen, setIsOpen] = useState(false);
+    const navigate = useNavigate();
     return (
         <div className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 border-b border-borderColor relative transition-all ${location.pathname} ==="/" && "bg-light"`}>
 
@@ -25,10 +26,14 @@ const Navbar = () => {
                 </div>
 
                 <div className='flex max-sm:flex-col items-start sm:items-center gap-6'>
-                    <button className='cursor-pointer'>Dashbord</button>
-                    <button className='cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transistion-all text-white rounded-lg'>Login</button>
+                    <button onClick={()=> navigate('/owner')} className='cursor-pointer'>Dashbord</button>
+                    <button onClick={()=> setShowLogin(true)} className='cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transistion-all text-white rounded-lg'>Login</button>
                 </div>
             </div>
+
+            <button className='sm:hidden cursor-pointer' aria-label='menu' onClick={()=> setIsOpen(!isOpen)}>
+                <img src={isOpen ? assets.close_icon : assets.menu_icon} alt="" />
+            </button>
         </div>
     )
 }
